c50563cc9133b973cab28f57289a64b4045a2a6e
AlexiWolf <alexiwolf@pm.me>
2022-10-02 19:55:47 -0500

    Translated basic settings to lua

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+local set = vim.opt
+set.compatible = false 
+set.errorbells = false 
+
+set.number = true
+set.relativenumber = true
+
+set.smartindent = true
+set.scrolloff = 999
+
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+
+set.wrap = false 
+set.colorcolumn = {80}
