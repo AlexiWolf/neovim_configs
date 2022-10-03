f0b40f7a3be02442b8275614232270ae477c5d45
AlexiWolf <alexiwolf@pm.me>
2022-10-02 23:47:18 -0500

    Translated plugins to lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,4 +2,14 @@ local Plug = vim.fn["plug#"]
 
 vim.call("plug#begin")
 
+Plug("easymotion/vim-easymotion")
+Plug("gpanders/editorconfig.nvim")
+Plug("itchyny/lightline.vim")
+Plug("junegunn/fzf.vim")
+Plug("kassio/neoterm")
+Plug("ludovicchabant/vim-gutentags")
+Plug("neoclide/coc.nvim", {branch = "release"})
+Plug("tpope/vim-fugitive")
+Plug("vim-test/vim-test")
+
 vim.call("plug#end")
