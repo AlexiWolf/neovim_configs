412cba00f0d4c909c8927a2e627a91a4bcbf805c
AlexiWolf <alexiwolf@pm.me>
2022-10-05 02:45:07 -0500

    Added neoterm settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 local set = vim.opt
 
-set.compatible = false 
-set.errorbells = false 
+set.compatible = false
+set.errorbells = false
 
 set.number = true
 set.relativenumber = true
@@ -14,10 +14,10 @@ set.softtabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
 
-set.wrap = false 
+set.wrap = false
 set.colorcolumn = {80}
 
-set.timeoutlen = 0 
+set.timeoutlen = 0
 
 vim.cmd("hi Pmenu ctermbg=16, ctermfg=15")
 
@@ -25,4 +25,5 @@ require("plugins")
 require("whichkey")
 require("coc")
 require("keybindings")
+require("neoterm")
 
