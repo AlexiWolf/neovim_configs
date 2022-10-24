251e8df99e90a649603b9596a127ef3c763d9fa3
AlexiWolf <alexiwolf@pm.me>
2022-10-24 16:34:48 -0500

    Configured cmp

--- nvim/lua/cmp_config.lua
+++ nvim/lua/cmp_config.lua
@@ -5,21 +5,20 @@ cmp.setup {
         expand = function(args) end,
     },
     window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
-    mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.scroll_docs(-4),
-        ['<S-Tab>'] = cmp.mapping.scroll_docs(4),
-        ['<Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+    mapping = {
+        ["<Tab>"] = cmp.mapping.select_next_item({behavior=cmp.SelectBehavior.Insert}),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({behavior=cmp.SelectBehavior.Insert}),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    },
     sources = cmp.config.sources(
         {
-            { name = 'nvim_lsp' },
+            { name = "nvim_lsp" },
         }, {
-            { name = 'buffer' },
+            { name = "buffer" },
         }
     )
 }
