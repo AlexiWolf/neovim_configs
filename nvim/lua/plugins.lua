57b4b48725f49b4194690ec643db75ace5e650af
AlexiWolf <alexiwolf@pm.me>
2022-11-05 02:58:03 -0500

    Added `luasnip` and `friendly-snippets`

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,5 +22,7 @@ Plug("hrsh7th/cmp-nvim-lsp")
 Plug("hrsh7th/cmp-buffer")
 Plug("hrsh7th/cmp-path")
 Plug("hrsh7th/cmp-cmdline")
+Plug("L3MON4D3/LuaSnip")
+Plug("rafamadriz/friendly-snippets")
 
 vim.call("plug#end")
