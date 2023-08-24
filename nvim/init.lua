5a9f3a740cdd87c6b6ca6fb80ba3bf4682977aa3
AlexiWolf <alexi@wolvesin.space>
2023-08-24 09:43:12 -0500

    Removed `mason` plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,6 +35,5 @@ require("fzf_config")
 require("treesitter_config")
 require("luasnip_config")
 require("cmp_config")
-require("mason_config")
 
 vim.cmd("colorscheme duskfox")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -26,6 +26,5 @@ Plug("hrsh7th/cmp-path")
 Plug("hrsh7th/cmp-cmdline")
 Plug("L3MON4D3/LuaSnip")
 Plug("rafamadriz/friendly-snippets")
-Plug("williamboman/mason.nvim")
 
 vim.call("plug#end")
