780a5c841fe71d622ee20e09d46bd2f2f498c501
AlexiWolf <alexi@wolvesin.space>
2023-08-25 01:13:34 -0500

    Added `which-key` config file

---
+++ nvim/lua/config/which_key.lua
@@ -0,0 +1,12 @@
+return {
+    WHICH_KEY_CONFIG = {
+        icons = {
+            breadcrumb = ">>",
+            separator = "->",
+        },
+        spelling = {
+            enabled = true,
+            suggestions = 20,
+        }
+    }
+}
