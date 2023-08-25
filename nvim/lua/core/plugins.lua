abd9b1f1a2f7182d4653840742b096a90c468c30
AlexiWolf <alexi@wolvesin.space>
2023-08-25 03:16:42 -0500

    Added `mason.nvim` plugin

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -4,5 +4,6 @@ vim.call("plug#begin")
     Plug("tpope/vim-fugitive")
     Plug("folke/which-key.nvim")
     Plug("EdenEast/nightfox.nvim")
+    Plug("williamboman/mason.nvim")
 vim.call("plug#end")
 
