a274354c381d8111cd3cf698ec5e1b424fff522a
AlexiWolf <alexi@wolvesin.space>
2023-08-25 00:44:02 -0500

    Moved Nvim settings to `config/settings.lua`

---
+++ nvim/lua/config/settings.lua
@@ -0,0 +1,25 @@
+local set = vim.opt
+
+set.compatible = false
+set.errorbells = false
+
+set.number = true
+set.relativenumber = true
+
+set.smartindent = true
+set.scrolloff = 999
+
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+
+set.wrap = false
+set.colorcolumn = {79, 99, 119}
+
+set.timeoutlen = 1000
+
+set.spell = true
+set.spelllang = { "en_us" }
+set.spelloptions = "camel"
+set.smartcase = true
