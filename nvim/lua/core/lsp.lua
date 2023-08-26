caf27a80dbcd8afd332571d2d8e6492e22595a05
AlexiWolf <alexi@wolvesin.space>
2023-08-26 10:00:18 -0500

    Added get-or-default function for lsp configs

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -3,8 +3,17 @@ local mason_lspconfig = require("mason-lspconfig")
 
 local lsp_settings = {}
 
+function get_lsp_config_or_default(lsp_settings, server_name)
+    local config = lsp_settings[server_name]
+    if config == nil then
+        return config
+    else
+        return lsp_settings["default"]
+    end
+end
+
 mason_lspconfig.setup_handlers({
     function(server_name)
-        lspconfig[server_name].setup(lsp_settings[server_name])
+        lspconfig[server_name].setup(get_lsp_config_or_default(lsp_settings, server_name))
     end,
 })
