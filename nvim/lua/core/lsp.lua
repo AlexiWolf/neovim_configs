e4d57692aeae6fc4c2d7c282054cce3552549ee1
AlexiWolf <alexi@wolvesin.space>
2023-08-26 15:57:18 -0500

    Added function for creating valid configs
    It essentially reads the server config, and uses the default settings
    when it can, or overrides the default value when values are provided.

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -3,18 +3,25 @@ local mason_lspconfig = require("mason-lspconfig")
 
 local LSP_CONFIGS = require("config.lsp")
 
-local function build_lsp_config(lsp_configs, server_name)
-    local config = lsp_configs[server_name]
-    if config == nil then
-        return lsp_configs["default"]
+local function get_config_field_or_default(config, setting_name)
+    if config == nil or config[setting_name] == nil then
+        return LSP_CONFIGS["default"][setting_name]
     else
-        return config
+        return config[setting_name]
     end
+end
+
+local function build_lsp_config(server_name)
+    local normalized_lsp_config = {}
+    local lsp_config = LSP_CONFIGS[server_name]
+    normalized_lsp_config["capabilities"] = get_config_field_or_default(lsp_config, "capabilities")
+    normalized_lsp_config["on_attach"] = get_config_field_or_default(lsp_config, "on_attach")
+    return normalized_lsp_config
 end
 
 local function default_setup_handler(server_name)
     local lsp = lspconfig[server_name]
-    local config = build_lsp_config(LSP_CONFIGS, server_name)
+    local config = build_lsp_config(server_name)
     lsp.setup(config)
 end
 
