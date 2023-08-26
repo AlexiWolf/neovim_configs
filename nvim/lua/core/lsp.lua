0944ebe2ebb3d9937ad16e6f3e58a851a79ccec2
AlexiWolf <alexi@wolvesin.space>
2023-08-26 15:16:08 -0500

    Fixed indents

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -13,9 +13,9 @@ local function get_lsp_config_or_default(lsp_configs, server_name)
 end
 
 local function default_setup_handler(server_name)
-        local lsp = lspconfig[server_name]
-        local config = get_lsp_config_or_default(LSP_CONFIGS, server_name)
-        lsp.setup(config)
+    local lsp = lspconfig[server_name]
+    local config = get_lsp_config_or_default(LSP_CONFIGS, server_name)
+    lsp.setup(config)
 end
 
 mason_lspconfig.setup_handlers({
