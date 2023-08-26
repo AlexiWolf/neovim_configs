689cc721b86602c017fff61e521819244cbfa584
AlexiWolf <alexi@wolvesin.space>
2023-08-26 10:20:15 -0500

    Fixed `rust_analyzer` config

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,14 +1,27 @@
+local which_key = require("which-key")
+
+local keymap = require("config.keymap")
+
+local function register_common_lsp_keymap()
+    which_key.register(keymap["lsp_common"])
+end
+
 local config = {
     default = {},
     ["rust_analyzer"] = {
-        cargo = {
-            buildScripts = {
-                enable = true,
+        on_attach = register_common_lsp_keymap,
+        settings = {
+            ["rust_analyzer"] = {
+                cargo = {
+                    buildScripts = {
+                        enable = true,
+                    },
+                    features = "all"
+                },
+                procMacro = {
+                    enable = true
+                },
             },
-            features = "all"
-        },
-        procMacro = {
-            enable = true
         },
     },
 }
