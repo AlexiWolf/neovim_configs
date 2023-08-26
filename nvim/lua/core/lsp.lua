518bd1a2574046f5ca5e14118ff600683943b5e6
AlexiWolf <alexi@wolvesin.space>
2023-08-26 15:49:02 -0500

    Renamed function

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -3,7 +3,7 @@ local mason_lspconfig = require("mason-lspconfig")
 
 local LSP_CONFIGS = require("config.lsp")
 
-local function get_lsp_config_or_default(lsp_configs, server_name)
+local function build_lsp_config(lsp_configs, server_name)
     local config = lsp_configs[server_name]
     if config == nil then
         return lsp_configs["default"]
@@ -14,7 +14,7 @@ end
 
 local function default_setup_handler(server_name)
     local lsp = lspconfig[server_name]
-    local config = get_lsp_config_or_default(LSP_CONFIGS, server_name)
+    local config = build_lsp_config(LSP_CONFIGS, server_name)
     lsp.setup(config)
 end
 
