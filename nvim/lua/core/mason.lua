b6752812deebc21d2e39e4b2ea38c58982e2eadb
AlexiWolf <alexi@wolvesin.space>
2023-08-26 08:40:57 -0500

    Import, and use the mason config

--- nvim/lua/core/mason.lua
+++ nvim/lua/core/mason.lua
@@ -1,5 +1,7 @@
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
+local mason_config = require("config.mason")
+
 mason.setup()
-mason_lspconfig.setup()
+mason_lspconfig.setup(mason_config.lsp_config)
