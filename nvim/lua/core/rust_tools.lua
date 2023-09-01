569fb71abb995a53cfa5e4a8e892e8ed4b099e80
AlexiWolf <alexi@wolvesin.space>
2023-09-01 01:07:43 -0500

    Use the config when setting up `rust-tools`

--- nvim/lua/core/rust_tools.lua
+++ nvim/lua/core/rust_tools.lua
@@ -1,6 +1,8 @@
+local rust_tools = require("rust-tools")
+local config = require("config.rust_tools")
+
 local function rust_setup_handler()
-    local rust_tools = require("rust-tools")
-    rust_tools.setup()
+    rust_tools.setup(config)
     rust_tools.inlay_hints.set()
     rust_tools.runnables.runnables()
     rust_tools.hover_actions.hover_actions()
