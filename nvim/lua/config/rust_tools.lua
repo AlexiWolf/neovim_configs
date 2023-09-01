a254a67e6e60a4e41174324f6e62f84a82cecb73
AlexiWolf <alexi@wolvesin.space>
2023-09-01 02:21:38 -0500

    Added missing `extension` folder to path

--- nvim/lua/config/rust_tools.lua
+++ nvim/lua/config/rust_tools.lua
@@ -2,7 +2,7 @@ local mason_registry = require("mason-registry")
 local codelldb = mason_registry.get_package("codelldb")
 local rust_tools_dap = require("rust-tools.dap")
 
-local codelldb_install_path = codelldb:get_install_path()
+local codelldb_install_path = codelldb:get_install_path() .. "/extension/"
 local codelldb_path = codelldb_install_path .. "adapter/codelldb"
 local liblldb_path = codelldb_install_path .. "lldb/lib/liblldb.so"
 
